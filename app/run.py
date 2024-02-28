619f10766f8282dd2567c1b61d0653ef5b6ebafb
Cp0204 <Cp0204@qq.com>
2024-02-28 17:16:55 +0800

    固定底栏，显示版本号

--- app/run.py
+++ app/run.py
@@ -19,6 +19,7 @@ import os
 config_path = os.environ.get("CONFIG_PATH", "./config/quark_config.json")
 
 app = Flask(__name__)
+app.config["APP_VERSION"] = "0.2.1"
 app.secret_key = "ca943f6db6dd34823d36ab08d8d6f65d"
 app.json.ensure_ascii = False
 app.json.sort_keys = False
@@ -94,7 +95,7 @@ def logout():
 def index():
     if not is_login():
         return redirect(url_for("login"))
-    return render_template("index.html")
+    return render_template("index.html", version=app.config["APP_VERSION"])
 
 
 # 获取配置数据
