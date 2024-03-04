8341aaf296b3c71bac396e1b8872dcf019a94405
Cp0204 <Cp0204@qq.com>
2024-03-04 15:16:15 +0800

    v0.2.3

--- app/run.py
+++ app/run.py
@@ -19,7 +19,7 @@ import os
 config_path = os.environ.get("CONFIG_PATH", "./config/quark_config.json")
 
 app = Flask(__name__)
-app.config["APP_VERSION"] = "0.2.2"
+app.config["APP_VERSION"] = "0.2.3"
 app.secret_key = "ca943f6db6dd34823d36ab08d8d6f65d"
 app.json.ensure_ascii = False
 app.json.sort_keys = False
