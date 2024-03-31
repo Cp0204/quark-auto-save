8830f6b94315401b0a72fdbd422cea8b79504238
Cp0204 <Cp0204@qq.com>
2024-03-31 18:50:44 +0800

    细节修复优化

--- app/run.py
+++ app/run.py
@@ -25,12 +25,12 @@ script_path = os.environ.get("SCRIPT_PATH", "./quark_auto_save.py")
 config_path = os.environ.get("CONFIG_PATH", "./config/quark_config.json")
 
 app = Flask(__name__)
-app.config["APP_VERSION"] = "0.2.5"
+app.config["APP_VERSION"] = "0.2.5.1"
 app.secret_key = "ca943f6db6dd34823d36ab08d8d6f65d"
 app.json.ensure_ascii = False
 app.json.sort_keys = False
-app.jinja_env.variable_start_string = '[['
-app.jinja_env.variable_end_string = ']]'
+app.jinja_env.variable_start_string = "[["
+app.jinja_env.variable_end_string = "]]"
 
 
 def gen_md5(string):
@@ -176,6 +176,7 @@ def reload_tasks():
             scheduler.resume()  # 恢复调度器
     else:
         print(">>> no crontab")
+
 
 def init():
     # 检查配置文件是否存在
