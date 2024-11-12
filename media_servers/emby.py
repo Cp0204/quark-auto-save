dd42197b277c825536e56ae999130e254a03aafb
Cp0204 <Cp0204@qq.com>
2024-11-13 03:39:18 +0800

    ♻️ 优化Emby模块配置及加载逻辑

--- media_servers/emby.py
+++ media_servers/emby.py
@@ -2,17 +2,24 @@ import requests
 
 
 class Emby:
+
+    default_config = {"url": "", "apikey": ""}
+
     def __init__(self, **kwargs):
         self.is_active = False
-        if kwargs.get("url") and kwargs.get("apikey"):
-            self.emby_url = kwargs.get("url")
-            self.emby_apikey = kwargs.get("apikey")
-            if self.get_info():
-                self.is_active = True
+        if kwargs:
+            for key, value in self.default_config.items():
+                if key in kwargs:
+                    setattr(self, key, kwargs[key])
+                else:
+                    print(f"{self.__class__.__name__} 模块缺少必要参数: {key}")
+            if self.url and self.apikey:
+                if self.get_info():
+                    self.is_active = True
 
     def get_info(self):
-        url = f"{self.emby_url}/emby/System/Info"
-        headers = {"X-Emby-Token": self.emby_apikey}
+        url = f"{self.url}/emby/System/Info"
+        headers = {"X-Emby-Token": self.apikey}
         querystring = {}
         response = requests.request("GET", url, headers=headers, params=querystring)
         if "application/json" in response.headers["Content-Type"]:
@@ -27,8 +34,8 @@ class Emby:
 
     def refresh(self, emby_id):
         if emby_id:
-            url = f"{self.emby_url}/emby/Items/{emby_id}/Refresh"
-            headers = {"X-Emby-Token": self.emby_apikey}
+            url = f"{self.url}/emby/Items/{emby_id}/Refresh"
+            headers = {"X-Emby-Token": self.apikey}
             querystring = {
                 "Recursive": "true",
                 "MetadataRefreshMode": "FullRefresh",
@@ -48,8 +55,8 @@ class Emby:
 
     def search(self, media_name):
         if media_name:
-            url = f"{self.emby_url}/emby/Items"
-            headers = {"X-Emby-Token": self.emby_apikey}
+            url = f"{self.url}/emby/Items"
+            headers = {"X-Emby-Token": self.apikey}
             querystring = {
                 "IncludeItemTypes": "Series",
                 "StartIndex": 0,
