16866a00b951c0310066eae61473c7c314fc98e5
Cp0204 <Cp0204@qq.com>
2024-11-29 17:17:29 +0800

    ♻️ 重构对通知变量 bool 值的处理

--- notify.py
+++ notify.py
@@ -181,7 +181,8 @@ def console(title: str, content: str) -> None:
     """
     使用 控制台 推送消息。
     """
-    print(f"{title}\n\n{content}")
+    if str(push_config.get("CONSOLE")).lower() != "false":
+        print(f"{title}\n\n{content}")
 
 
 def dingding_bot(title: str, content: str) -> None:
